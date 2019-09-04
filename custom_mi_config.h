8baa67532a645b887513a05a70b7d6a9b4dc17cb
tuchucheng <tuchucheng@xiaomi.com>
2019-09-04 15:58:45 +0800

    new branch Dialog

---
+++ custom_mi_config.h
@@ -0,0 +1,13 @@
+/***************************************************************************//**
+ * @file
+ * @brief custom_mi_config.h
+ *
+ ******************************************************************************/
+
+#ifndef CUSTOM_MI_CONFIG_H
+#define CUSTOM_MI_CONFIG_H
+
+#define MI_BLE_ENABLED
+#define HAVE_MSC            0
+
+#endif
